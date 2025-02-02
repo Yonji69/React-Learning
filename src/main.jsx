4ede1d2a14d089112663602a9d4da61e7010e089
Yonji69 <yonji745@gmail.com>
2025-02-02 06:14:13 +0530

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
+import Welcome from "./Home"; // Import the Home component
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <Welcome />
+  </StrictMode>
+);
+
+
+
+
